94a32dd877910bfbdb11847dee6340448624d488
Yfling <598184085@qq.com>
2017-05-06 16:27:03 +0800

    header组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -19,34 +19,59 @@
         </div>
       </div>
       <div v-if="seller.supports" class="support-count">
-        <span class="count" @click="showDetail">{{seller.supports.length}}个</span>
+        <span class="count" @click="detail">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
     <div class="bulletion-wrapper">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
-      <i class="icon-keyboard_arrow_right" @click="showDetail"></i>
+      <i class="icon-keyboard_arrow_right" @click="detail"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%" >
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+               <!-- :score、:size将header里面的score、size传进star组件 -->
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="detail">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 
 </template>
 
 <script type="text/javascript">
 // import star from '../../components/star/star.vue';
-// import star from '../star/star.vue';
+import star from '../star/star.vue';
 
 export default {
   props: {
@@ -55,7 +80,7 @@ export default {
     }
   },
   components: {
-    'star': star
+    star
   },
   data() {
     return {
@@ -63,7 +88,7 @@ export default {
     }
   },
   methods: {
-    showDetail() {
+    detail() {
       this.detailShow = !this.detailShow;
     }
   },
@@ -218,7 +243,26 @@ export default {
   width: 100%;
   height: 100%;
   overflow: auto;  /* 不能用hidden 因为如果一旦超过的部分就会被隐藏了 */
-  background: rgba(7, 17, 27, .8);
+  transition: all 0.5s;
+  background: rgba(7, 17, 27, 0.8);
+  backdrop-filter: blur(10px);
+  /* 过渡动画和1.0有区别，这里要注意一下 */
+  &.fade-enter-active {  /* 进入过渡的结束状态 */
+    opacity: 1;
+    background: rgba(7, 17, 27, 0.8);
+  }
+  &.fade-enter {  /* 进入过渡的开始状态 */
+    opacity: 0;
+    background: rgba(7, 17, 27, 0);
+  }
+  &.fade-leave {  /* 离开过渡的开始状态 */
+    opacity: 1;
+    background: rgba(7, 17, 27, 0.8);
+  }
+  &.fade-leave-active {  /* 离开过渡的结束状态 */
+    opacity: 0;
+    background: rgba(7, 17, 27, 0);
+  }
   .detail-wrapper {
     min-height: 100%;
     width: 100%;
@@ -230,6 +274,76 @@ export default {
         text-align: center;
         font-size: 16px;
         font-weight: 700;
+      }
+      .star-wrapper {
+        margin-top: 18px;
+        padding: 2px 0;
+        text-align: center;
+      }
+      .title {
+        display: flex;
+        width: 80%;
+        margin: 28px auto 24px auto;
+        .line {
+          flex: 1;
+          position: relative;
+          top: -6px;
+          border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+        }
+        .text {
+          padding: 0 12px;
+          font-weight: 700;
+          font-size: 14px;
+        }
+      }
+      .supports {
+        width: 80%;
+        margin: 0 auto;
+        .support-item {
+          padding: 0 12px;
+          margin-bottom: 12px;
+          font-size: 0;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .icon {
+            display: inline-block;
+            width: 16px;
+            height: 16px;
+            vertical-align: top;
+            margin-right: 6px;
+            background-size: 16px 16px;
+            background-repeat: no-repeat;
+            &.decrease {
+              @include bg-image('img/decrease_2');
+            }
+            &.discount {
+              @include bg-image('img/discount_2');
+            }
+            &.guarantee {
+              @include bg-image('img/guarantee_2');
+            }
+            &.invoice {
+              @include bg-image('img/invoice_2');
+            }
+            &.special {
+              @include bg-image('img/special_2');
+            }
+          }
+          .text {
+            line-height: 16px;
+            font-size: 12px;
+          }
+        }
+      }
+      .bulletin {
+        width: 80%;
+        margin: 0 auto;
+        .content {
+          padding: 0 12px;
+          line-height: 24px;
+          font-size: 12px;
+        }
       }
     }
   }
--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="star" :class="starType">
-        图片是放在css属性里面的，所以这里直接v-for的是属性
-        <span v-for="(itemClass,index) in itemClasses" :class="itemClass" class="star-item"></span>
+        lalalalala
+        <!-- 图片是放在css属性里面的，所以这里直接v-for的是属性 -->
+        <span v-for="(itemClass,index) in itemClasses" :class="itemClass" class="star-item" :key="itemClass.index"></span>
     </div>
 </template>
 
@@ -12,7 +13,7 @@
   const CLS_OFF = 'off';
 
   export default {
-    props: {
+    props: {  // props里面是父级传过来的两个参数
       size: {
         type: Number
       },
@@ -24,7 +25,7 @@
       starType() {
         return 'star-' + this.size;
       },
-      itemCLasses() {
+      itemClasses() {
         let result = [];  // 用数组来存放属性的名字
         let score = Math.floor(this.score * 2) / 2;
         let hasDecimal = score % 1 != 0;  // 判断是否有小数
@@ -57,64 +58,64 @@
     .star-item {
       display: inline-block;
       background-repeat: no-repeat;
-      .star-48 {
-        .star-item {
-          width: 20px;
-          height: 20px;
-          margin-right: 22px;
-          background-size: 20px 20px;
-          &:last-child {
-            margin-right: 0;
-          }
-          &.on {
-            @include bg-image('star48_on');
-          }
-          &.half {
-            @include bg-image('star48_half');
-          }
-          &.off {
-            @include bg-image('star48_off');
-          }
+    }
+    &.star-48 {
+      .star-item {
+        width: 20px;
+        height: 20px;
+        margin-right: 22px;
+        background-size: 20px 20px;
+        &:last-child {
+          margin-right: 0;
+        }
+        &.on {
+          @include bg-image('img/star48_on');
+        }
+        &.half {
+          @include bg-image('img/star48_half');
+        }
+        &.off {
+          @include bg-image('img/star48_off');
         }
       }
-      .star-36 {
-        .star-item {
-          width: 15px;
-          height: 15px;
-          margin-right: 6px;
-          background-size: 15px 15px;
-          &:last-child {
-            margin-right: 0;
-          }
-          &.on {
-            @include bg-image('img/star36_on');
-          }
-          &.half {
-            @include bg-image('img/star36_half');
-          }
-          &.off {
-            @include bg-image('img/star36_off');
-          }
+    }
+    &.star-36 {
+      .star-item {
+        width: 15px;
+        height: 15px;
+        margin-right: 6px;
+        background-size: 15px 15px;
+        &:last-child {
+          margin-right: 0;
+        }
+        &.on {
+          @include bg-image('img/star36_on');
+        }
+        &.half {
+          @include bg-image('img/star36_half');
+        }
+        &.off {
+          @include bg-image('img/star36_off');
         }
       }
-      .star-24 {
-        .star-item {
-          width: 10px;
-          height: 10px;
-          margin-right: 3px;
-          background-size: 10px 10px;
-          &:last-child {
-            margin-right: 0;
-          }
-          &.on {
-            @include bg-image('img/star24_on');
-          }
-          &.half {
-            @include bg-image('img/star24_half');
-          }
-          &.off {
-            @include bg-image('img/star24_off');
-          }
+    }
+    &.star-24 {
+      .star-item {
+        width: 10px;
+        height: 10px;
+        margin-right: 3px;
+        background-size: 10px 10px;
+        &:last-child {
+          margin-right: 0;
+        }
+        &.on {
+          @include bg-image('img/star24_on');
+        }
+        &.half {
+          @include bg-image('img/star24_half');
+        }
+        &.off {
+          @include bg-image('img/star24_off');
         }
       }
     }
